1af315ce26227d67d13c222c2a208b69d3879fa2
Ageeva Anastasiia Artemovna <ageeva@ccfit.nsu.ru>
2017-12-23 15:05:42 +0700

    a

--- lab2_2.sh
+++ lab2_2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget http://factorized.net/patterns.txt 
-egrep -w "\b[:alpha:]*\b" patterns.txt
-egrep -w "\b[:digit:]*\b" patterns.txt
+egrep -w "\b[A-z]*\b" patterns.txt
+egrep -w "\b[0-9]*\b" patterns.txt
 
